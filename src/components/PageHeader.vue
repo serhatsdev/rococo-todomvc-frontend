1443688d4b7f6bbfdafe71bc4484d94263ebe139
Serhat S <68604417+serhatsdev@users.noreply.github.com>
2025-04-08 17:24:43 +0300

    add todomvc ui

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps({
+  text: String,
+});
+</script>
+
+<template>
+  <h1>{{ text }}</h1>
+</template>
+
+<style scoped>
+h1 {
+  margin: 20px 0;
+  font-size: 80px;
+  font-weight: 200;
+  text-align: center;
+  color: #b83f45;
+}
+</style>
---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,23 @@
+<script setup>
+</script>
+<template>
+  <header class="header">
+    <input
+            type="text"
+            class="new-todo"
+            autofocus
+            autocomplete="off"
+            placeholder="What needs to be done?"
+            @keyup.enter="
+                // @ts-ignore
+                $emit('add-todo', $event.target.value);
+                // @ts-ignore
+                $event.target.value = '';
+            "
+      />
+  </header>
+</template>
+
+<style scoped>
+
+</style>
