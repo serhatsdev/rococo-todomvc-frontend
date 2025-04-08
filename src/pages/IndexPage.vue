eabe1def0eaac64ff7841adb59b8f1b6a37a4f85
Serhat S <68604417+serhatsdev@users.noreply.github.com>
2025-04-09 00:50:19 +0300

    add footer in IndexPage

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -6,4 +6,16 @@ import TodosComponent from '@/components/TodosComponent.vue'
 <template>
   <PageHeader text="todos" />
   <TodosComponent />
+  <footer class="info">
+      <p>Double-click to edit a todo</p>
+      <p>Created by the Serhat S.</p>
+      <p>Part of <a href="https://github.com/ecorrouge">Ecor Rouge</a></p>
+  </footer>
 </template>
+
+<style scoped>
+.info p {
+  line-height: 1;
+  margin: 11px;
+}
+</style>
